4811e79e5dc0ccec6b47580c51aef399e61942dc
Hyouketsu <bnooranii@gmail.com>
2023-03-09 21:44:47 -0800

    items

--- server.js
+++ server.js
@@ -126,6 +126,115 @@ app.put('/employees/:id', (req, res) => {
 // customer
 
 
+// Items
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/items.html');
+});
+
+app.get('/items/:id', (req, res) => {
+  const { id } = req.params;
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting connection from db', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    const sql = 'SELECT * FROM items WHERE itemID = ?';
+    connection.query(sql, [id], (err, results) => {
+      connection.release();
+      if (err) {
+        console.error('Error executing query:', err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      if (results.length === 0) {
+        return res.status(404).json({ error: 'Employee not found' });
+      }
+      const employee = results[0];
+      res.json(employee);
+    });
+  });
+});
+
+
+app.get('/items', (req, res) => {
+    pool.getConnection((err, connection) => {
+        if (err) {  
+            console.error('Error getting connection from db', err);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+        const sql = 'SELECT itemID, name, price, restriction FROM items';
+        connection.query(sql, (err, results) => {
+            connection.release();
+            if (err) {
+                console.error('Error executing query:', err);
+                return res.status(500).json({ error: 'Internal server error' });
+            }
+        res.json(results);
+        });
+    });
+});
+
+
+app.post('/add-item', (req, res) => {
+  const { name, price, restriction} = req.body;
+  pool.getConnection((err, connection) => {
+    if (err) {  
+      console.error('Error getting connection from db', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    const sql = 'INSERT INTO items (name, price, restriction VALUES (?, ?, ?)';
+    connection.query(sql, [name, price, restriction], (err, results) => {
+      connection.release();
+      if (err) {
+        console.error('Error executing query:', err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      res.redirect('/items.html');
+    });
+  });
+});
+
+app.delete('/items/:id', (req, res) => {
+  const { id } = req.params;
+  pool.getConnection((err, connection) => {
+    if (err) {  
+      console.error('Error getting connection from db', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    const sql = 'DELETE FROM items WHERE itemID = ?';
+    connection.query(sql, [id], (err, results) => {
+      connection.release();
+      if (err) {
+        console.error('Error executing query:', err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      res.sendStatus(204);
+    });
+  });
+});
+
+app.put('/items/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, price, restriction} = req.body;
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error('Error getting connection from db', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+    const sql = 'UPDATE items SET name=?, price=?, restriction=?';
+    connection.query(sql, [name, price, restriction], (err, results) => {
+      connection.release();
+      if (err) {
+        console.error('Error executing query:', err);
+        return res.status(500).json({ error: 'Internal server error' });
+      }
+      console.log('Query results:', results);
+      res.sendStatus(204);
+    });
+  });
+});
+
+
+
 const port = 5205;
 app.listen(port, () => {
   console.log('Server listening on port 5205');
